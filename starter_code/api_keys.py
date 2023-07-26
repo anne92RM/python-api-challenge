93dd59de49e5b9b975c192755799d5d2d00c4a37
anne92RM <annely.manguera@lowes.com>
2023-07-26 14:36:16 -0400

    add

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8e7271fbe13721c0e6db54deabd15f1f"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a35af0a174064464a9b8d9ee8a181ae0"
